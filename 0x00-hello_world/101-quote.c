32956b9770959d47673bcbd4129db949905de70f
Medhat <medhatmohammed1612@gmail.com>
2023-06-17 06:30:40 +0000

    Add script generates the assembly code (Intel syntax) of a C code and save it in an output file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - This is the entry point
+ * Return: 0 if the main runs successfully
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"); 
+	return (0);
+}
