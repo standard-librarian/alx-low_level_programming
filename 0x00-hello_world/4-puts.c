0edf10148f56d4d2a6930f05856136c0338d128b
Medhat <medhatmohammed1612@gmail.com>
2023-06-17 05:53:24 +0000

    Puts string in the stdo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 
+/**
+ * main - This is the entry point
+ * Return: 0 if the main runs successfully
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
