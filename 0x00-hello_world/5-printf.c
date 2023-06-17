2fcbdaccfd2a0c9d7075b0d8413dd2df2a3fccd3
Medhat <medhatmohammed1612@gmail.com>
2023-06-17 05:57:24 +0000

    Use printf instead of puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - This is the entry point
+ * Return: 0 if the main runs successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
