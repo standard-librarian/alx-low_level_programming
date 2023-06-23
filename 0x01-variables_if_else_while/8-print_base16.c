a4eb3c068799ddfa09e81870eef570695577a669
Medhat <medhatmohammed1612@gmail.com>
2023-06-23 14:40:08 +0000

    Print 0..9 and a..f

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This is the entry point
+ * Return: 0 if the main runs successfully
+ */
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	
+	putchar('\n');
+	return (0);
+}
