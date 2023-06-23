e0d69a8414a591fad1fc4e7ae134fa4751536ef2
Medhat <medhatmohammed1612@gmail.com>
2023-06-23 14:05:10 +0000

    Print 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This is the entry point
+ * Return: 0 if the main runs successfully
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	puts('\n');
+	return (0);
+}
