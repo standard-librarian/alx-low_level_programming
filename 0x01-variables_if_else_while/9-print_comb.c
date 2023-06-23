ba1f36430981e492a61acc9542204a76f5e8652a
Medhat <medhatmohammed1612@gmail.com>
2023-06-23 15:21:36 +0000

    0, 1, ..., 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print 0, 1, - 9
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		putchar(i + 48);
+
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		++i;
+	}
+	putchar('\n');
+
+	return (0);
+}
