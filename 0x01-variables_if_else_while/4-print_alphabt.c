aa641c3e02d11f2b7ef3dfb806b01e60bd646d65
Medhat <medhatmohammed1612@gmail.com>
2023-06-23 05:06:45 +0000

    Print the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - This is the entry point
+ * Return: 0 if the main runs successfully
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
