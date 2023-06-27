3b0629214fbabd4bfa041123c3ca0b6acac82b0b
Medhat <medhatmohammed1612@gmail.com>
2023-06-27 12:50:11 +0000

    Add _puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * _puts - a version of the c strlen
+ * @s: pointer to the string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
+
+#ifdef TEST
+/**
+ * main - test main function, gets called from the bash cmd "gcc -DTEST"
+ * Return:§Always 0
+ */
+int main(void)
+{
+	char s[] = "Hello, world!\n";
+
+	_puts(s);
+}
+#endif
