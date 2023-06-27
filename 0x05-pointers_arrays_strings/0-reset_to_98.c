12e0682028bbbc6d0fd0c984306ad0c0f4948080
Medhat <medhatmohammed1612@gmail.com>
2023-06-27 09:48:51 +0000

    Fix betty docs

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * reset-_to_98
- *
+ * reset_to_98 - change the value of the var pointed to by n
+ * @n: pointer to int
  * Return: void
  */
-void reset_to_98(int *n){
+void reset_to_98(int *n)
+{
 	*n = 98;
-	return;
 }
