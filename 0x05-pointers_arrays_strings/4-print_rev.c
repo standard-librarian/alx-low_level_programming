9ade78f5f514695e055fda3b794aaaa2e1bf5df1
Medhat <medhatmohammed1612@gmail.com>
2023-06-27 13:00:59 +0000

    Add print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - a version of the c slen
+ * @s: pointer to the sing
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = strlen(s); i >= 0; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
+
+#ifdef TEST
+/**
+ * main - test main function, gets called from the bash cmd "gcc -DTEST"
+ * Return:§Always 0
+ */
+int main(void)
+{
+	char s[] = "Hello, world!\n";
+
+	print_rev(s);
+}
+#endif
