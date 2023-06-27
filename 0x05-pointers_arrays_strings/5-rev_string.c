4d7a9a5058993e315f6e64316f1c39305f4811c1
Medhat <medhatmohammed1612@gmail.com>
2023-06-27 13:23:38 +0000

    String reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - a version of the c slen
+ * @s: pointer to the sing
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i;
+	char tmp;
+
+	for (i = 0; i < strlen(s) / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[strlen(s) - 1 - i];
+		s[strlen(s) - 1 - i] = tmp;
+	}
+}
+
+#ifdef TEST
+/**
+ * main - test main function, gets called from the bash cmd "gcc -DTEST"
+ * Return:§Always 0
+ */
+int main(void)
+{
+	char s[] = "Hello, world!\n";
+
+	rev_string(s);
+	printf("%s\n", s);
+}
+#endif
