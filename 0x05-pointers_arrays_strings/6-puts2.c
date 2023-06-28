0e7aa24e98471cd47f3c9d751507c90a38ff7617
Medhat <medhatmohammed1612@gmail.com>
2023-06-28 22:37:15 +0000

    Add puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * puts2 - a version of the c strlen
+ * @str: pointer to the string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+}
+
+#ifdef TEST
+/**
+ * main - test main function, gets called from the bash cmd "gcc -DTEST"
+ * Return:§Always 0
+ */
+int main(void)
+{
+	char s1[] = "Hello, world!\n";
+	char s2[] = "Hello, world\n";
+
+
+	puts2(s1);
+	puts2(s2);
+}
+#endif
