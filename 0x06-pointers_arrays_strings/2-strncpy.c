c4f44d5e67728f1b2eeb5f9c91130a77bb3104f4
Medhat <medhatmohammed1612@gmail.com>
2023-06-29 23:01:36 +0000

    Fix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,7 +16,12 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; *(src + i) != '\0' && i < n; i++)
 		*(dest + i) = *(src + i);
-	*(dest + i) = '\0';
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 
 }
