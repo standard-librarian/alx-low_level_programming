f519556d268f18df0edd365d4c1cc13018ac9d3f
Medhat <medhatmohammed1612@gmail.com>
2023-06-29 22:46:28 +0000

    Add docs

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * _strcat - function that concatenates
+ *          two strings.
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+*/
 char *_strcat(char *dest, char *src)
 {
 	int i, l = 0;
@@ -7,7 +16,7 @@ char *_strcat(char *dest, char *src)
 	for (i = 0; *(dest + i) != '\0'; i++)
 		l++;
 
-	for(i = 0; *(src + i) != '\0'; i++)
+	for (i = 0; *(src + i) != '\0'; i++)
 		*(dest + l + i) = *(src + i);
 	*(dest + l + i) = '\0';
 	return (dest);
@@ -15,12 +24,19 @@ char *_strcat(char *dest, char *src)
 }
 
 #ifdef TEST
+/**
+ * main - function for testing
+ *
+ * Return: int
+*/
 int main(void)
 {
 	char s1[] = "0123456789";
 	char s2[] = "0123456789";
-	printf("%d\n",(int) strlen(s1));
+
+	printf("%d\n", (int) strlen(s1));
 	char *p = _strcat(s1, s2);
+
 	printf("%d\n", (int) strlen(p));
 	puts(p);
 	return (0);
