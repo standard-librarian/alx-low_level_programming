df1218bb8e6f44cffc5b255ab91a69bea63ef377
Medhat <medhatmohammed1612@gmail.com>
2023-06-29 05:06:06 +0000

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+int *_atoi(char *s)
+{
+	int i, neg, num;
+
+	for (i = 0; *(s + i) < '0' || *(s + i) > '9'; i++)
+	{
+		if (*(s + i) == '-')
+			neg++;
+	}
+
+	for (; *(s + i) > '0' && *(s + i) < '9'; i++)
+	{
+		num *= 10;
+		num += (int) (*(s + i) - '0');
+	}
+	num *= (-2 * (neg % 2 - 0.5));
+	return (num);
+}
