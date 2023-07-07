b514656e5835a29ef02c0c34a99a8751d6b5fe31
Medhat <medhatmohammed1612@gmail.com>
2023-07-07 20:01:46 +0000

    Again

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# a script that creates a static library
-# called liball.a from all the .c files
-# that are in the current directory
-
 gcc -c *.c
-ar rc libmy.a *.o
-echo y | rm -I *.o
+ar rc liball.a *.o
+rm -rf *.o
