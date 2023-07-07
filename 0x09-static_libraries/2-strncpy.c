bb500c2950a2868470561eb43a6f52a15128f157
Medhat <medhatmohammed1612@gmail.com>
2023-07-07 19:08:56 +0000

    Add all functions to the static lib

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,13 @@
+/**
+ * _strncpy - A function that copies a string.
+ *
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
+ *
+ * Return: @dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+}
