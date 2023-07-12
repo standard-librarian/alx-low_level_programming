c78082ea767a34ae0e98a6192dc0edac4452ac06
Medhat Mohammed <medhatmohammed1612@gmail.com>
2023-07-12 20:14:13 +0300

    Fix the betty style

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,12 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
  * main - check the code
+ *
  * @argc: counter of the cli args
  * @argv: the vector of the cli args
+ *
  * Return: Always 0.
  */
 
@@ -11,8 +14,8 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0;i < argc; i++) 
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
-	
+
 	return (0);
 }
