aa3b2540b27ff0a4ad2b4ff0b59baa39559b54d3
Medhat Mohammed <medhatmohammed1612@gmail.com>
2023-07-12 20:02:26 +0300

    Print the number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ * @argc: counter of the cli args
+ * @argv: the vector of the cli args
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%s\n", argc - 1);
+	return (0);
+}
