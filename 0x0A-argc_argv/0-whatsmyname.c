c9094db5585cdc315003922bb7ac5de1fb3b3933
Medhat Mohammed <medhatmohammed1612@gmail.com>
2023-07-12 19:56:07 +0300

    Add main desc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - check the code
+ * @argc: counter of the cli args
+ * @argv: the vector of the cli args
+ * Return: Always 0.
+ */
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argsv[0]);
